30890ecbed2e1e371c6837db5a5dde6bcf89e05a
Василиса <evstigney2010@gmail.com>
2019-07-27 20:41:55 +0300

    футер - сделала десктопную версию

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,12 @@
 var initMap = function () {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: {lat: 59.936271, lng: 30.321076},
+    center: {lat: 59.936448, lng: 30.321076},
     disableDefaultUI: true,
   });
 
   var marker = new google.maps.Marker ({
-    position: {lat: 59.935835, lng: 30.321055},
+    position: {lat: 59.935959, lng: 30.321002},
     icon: 'img/icon-map-marker.svg',
     map: map
   });
