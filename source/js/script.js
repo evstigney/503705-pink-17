9a7d5bf5694306e4c21b0bcb66a172dd4be6baf4
Василиса <evstigney2010@gmail.com>
2019-08-09 14:56:02 +0300

    сделала срытие меню в мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,43 @@
+/*---- главное меню ----*/
+
+var body = document.querySelector('body');
+var pageHeader = document.querySelector('.page-header');
+var buttonMenuClose = document.querySelector('.page-header__menu-toggle--close');
+var buttonMenuOpen = document.querySelector('.page-header__menu-toggle--open');
+var buttonMenuAccessibility = document.querySelector('.page-header__menu-toggle span.visually-hidden');
+var mainNav = document.querySelector('.main-nav');
+var mainLogo = document.querySelector('.page-header__logo');
+
+var hiddenMainNav = function () {
+  mainNav.style.display = 'none';
+};
+
+var changeMenuButtonToOpen = function () {
+  buttonMenuClose.classList.add('page-header__menu-toggle--open');
+  buttonMenuClose.classList.remove('page-header__menu-toggle--close');
+  buttonMenuClose.style.top = '89px';
+  buttonMenuClose.style.right = '21px';
+  buttonMenuAccessibility.textContent = 'Открыть меню';
+};
+
+var changeMainLogoToClosedMenu = function () {
+  mainLogo.style.top = '69px';
+};
+
+var changePageHeaderToClosedMenu = function () {
+  body.style.marginTop = '-71px';
+  pageHeader.classList.add('page-header__menu--closed');
+  pageHeader.classList.remove('page-header__menu--open');
+};
+
+buttonMenuClose.addEventListener('click', function () {
+  hiddenMainNav();
+  changeMenuButtonToOpen();
+  changeMainLogoToClosedMenu();
+  changePageHeaderToClosedMenu();
+});
+
+
 /*---- интерактивная карта ----*/
 
 var initMap = function () {
